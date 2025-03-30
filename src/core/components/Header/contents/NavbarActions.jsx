8d2f81c6f6b3d9f28f05ebd6fd762848f2da3d67
catchmeifyoucan99 <thanhphohochiminh5@gmail.com>
2025-03-30 23:15:32 +0700

    ui(navbar): add auth modal

--- src/core/components/Header/contents/NavbarActions.jsx
+++ src/core/components/Header/contents/NavbarActions.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { openModal } from '../../../store/auth/modalSlice.jsx';
+import LoginModal from '../../../../modules/auth/popup/LoginModal.jsx';
+import RegisterModal from '../../../../modules/auth/popup/RegisterModal.jsx';
 
 const NavbarActions = () => {
+
+  const dispatch = useDispatch();
+  const { isOpen, modalType } = useSelector((state) => state.modal);
+
+  const handleOpenLoginModal = () => {
+    dispatch(openModal('login'));
+  };
+
+  const handleOpenRegisterModal = () => {
+    dispatch(openModal('register'));
+  };
+
+  const authMethodChange = (type) => {
+    dispatch(openModal(type));
+  };
+
   return(
     <div className="font-inter-tight flex items-center gap-3 max-h-[42px]">
       <div className="pr-6 gap-3 flex items-center">
-        <a href="#" className="text-sm">Log in</a>
-        <a href="#" className="text-sm">Sign Up</a>
+        <button onClick={handleOpenLoginModal} className="text-sm text-primary hover:text-primary/80">Log in</button>
+        {isOpen && modalType === 'register' && <RegisterModal authMethodChange={() => authMethodChange('login')} />}
+
+        <button onClick={handleOpenRegisterModal} className="text-sm text-primary hover:text-primary/80">Sign Up</button>
+        {isOpen && modalType === 'login' && <LoginModal authMethodChange={() => authMethodChange('register')} />}
       </div>
 
       <button className="bg-black text-white px-4 h-[42px] max-h-[42px] text-sm hover:bg-black/80 rounded-lg">Be Pro</button>
--- src/core/pages/HomePage.jsx
+++ src/core/pages/HomePage.jsx
@@ -1,33 +1,11 @@
 import React from 'react';
-import { useSelector, useDispatch } from "react-redux";
-import { openModal } from '../store/auth/modalSlice.jsx';
-import LoginModal from '../../modules/auth/popup/LoginModal.jsx';
-import RegisterModal from '../../modules/auth/popup/RegisterModal.jsx';
 
 const HomePage = () => {
-  const dispatch = useDispatch();
-  const { isOpen, modalType } = useSelector((state) => state.modal);
-
-  const handleOpenLoginModal = () => {
-    dispatch(openModal('login'));
-  };
-
-  const handleOpenRegisterModal = () => {
-    dispatch(openModal('register'));
-  };
-
-  const authMethodChange = (type) => {
-    dispatch(openModal(type));
-  };
 
   return (
     <div>
       <h1 className="">Trang chủ</h1>
-      <button onClick={handleOpenLoginModal}>Mở Modal Đăng Nhập</button>
-      <button onClick={handleOpenRegisterModal}>Mở Modal Đăng Ký</button>
 
-      {isOpen && modalType === 'login' && <LoginModal authMethodChange={() => authMethodChange('register')} />}
-      {isOpen && modalType === 'register' && <RegisterModal authMethodChange={() => authMethodChange('login')} />}
     </div>
   );
 };

6ef4cc87659acc34a21dfe78c0cb86a2ccd1bcf3
catchmeifyoucan99 <thanhphohochiminh5@gmail.com>
2025-03-31 02:32:14 +0700

    refactor(navbar): reponsive ui

--- src/core/components/Header/Navbar.jsx
+++ src/core/components/Header/Navbar.jsx
@@ -1,23 +1,59 @@
-import React from "react";
-import NavbarMenu from './contents/NavbarMenu.jsx';
-import NavbarLogo from './contents/NavbarLogo.jsx';
-import NavbarSearch from './contents/NavbarSearch.jsx';
-import NavbarActions from './contents/NavbarActions.jsx';
+import React, { useState, useEffect } from "react";
+import NavbarMenu from "./contents/NavbarMenu.jsx";
+import NavbarLogo from "./contents/NavbarLogo.jsx";
+import NavbarSearch from "./contents/NavbarSearch.jsx";
+import NavbarActions from "./contents/NavbarActions.jsx";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { LuUserRoundCheck } from "react-icons/lu";
+import { debounce } from "lodash";
+import clsx from "clsx";
 
 const Navbar = () => {
+  const [screenSize, setScreenSize] = useState({
+    isIpad: window.innerWidth <= 1270,
+    isMobile: window.innerWidth <= 570,
+  });
+
+  useEffect(() => {
+    const handleResize = debounce(() => {
+      const width = window.innerWidth;
+      setScreenSize({
+        isIpad: width <= 1270,
+        isMobile: width <= 570,
+      });
+    }, 100);
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      handleResize.cancel();
+    };
+  }, []);
+
   return (
-    <nav className="navbar font-inter-tight text-sm text-primary flex items-center justify-between bg-gray-100 px-[66px] py-2">
-      {/* Logo */}
-      <NavbarLogo/>
+    <nav
+      className={clsx(
+        "navbar md:px-12 sm:px-7 xs:px-5 s:px-3 font-inter-tight text-sm text-primary flex items-center justify-between bg-gray-100 py-2",
+        screenSize.isIpad ? "xl:px-8" : "xl:px-16",
+      )}
+    >
+      {screenSize.isIpad ? (
+        <div className="flex items-center">
+          <RxHamburgerMenu className="mr-3" size={21} />
+          <NavbarLogo />
+        </div>
+      ) : (
+        <>
+          <NavbarLogo />
+          <NavbarMenu />
+        </>
+      )}
 
-      {/* Menu */}
-      <NavbarMenu/>
+      <NavbarSearch />
 
-      {/* Search Bar */}
-      <NavbarSearch/>
-
-      {/* Actions */}
-      <NavbarActions/>
+      <div>{screenSize.isMobile ? <LuUserRoundCheck size={21} className="ml-2" /> : <NavbarActions />}</div>
     </nav>
   );
 };
--- src/core/components/Header/contents/NavbarActions.jsx
+++ src/core/components/Header/contents/NavbarActions.jsx
@@ -22,17 +22,19 @@ const NavbarActions = () => {
   };
 
   return(
-    <div className="font-inter-tight flex items-center gap-3 max-h-[42px]">
-      <div className="pr-6 gap-3 flex items-center">
-        <button onClick={handleOpenLoginModal} className="text-sm text-primary hover:text-primary/80">Log in</button>
+    <div className="xxl:ml-0 ml-2 font-inter-tight flex items-center gap-3 max-h-[42px]">
+      <div className="xxl:pr-4 lg:pr-2 pr-0 gap-3 flex items-center">
+        <button onClick={handleOpenLoginModal} className="xl:mr-2 mr-0 text-sm text-primary hover:text-primary/80 text-nowrap">Log in</button>
         {isOpen && modalType === 'register' && <RegisterModal authMethodChange={() => authMethodChange('login')} />}
 
-        <button onClick={handleOpenRegisterModal} className="text-sm text-primary hover:text-primary/80">Sign Up</button>
+        <button onClick={handleOpenRegisterModal} className="text-sm text-primary hover:text-primary/80 text-nowrap">Sign Up</button>
         {isOpen && modalType === 'login' && <LoginModal authMethodChange={() => authMethodChange('register')} />}
       </div>
 
-      <button className="bg-black text-white px-4 h-[42px] max-h-[42px] text-sm hover:bg-black/80 rounded-lg">Be Pro</button>
-      <button className="border border-black px-4 h-[42px] max-h-[42px] hover:bg-black/80 hover:text-white text-sm rounded-lg">Submit Website</button>
+     <div className="flex xxl:ml-2 ml-0 items-center gap-3">
+       <button className="lg:ml-0 md:ml-4 sm:flex hidden items-center bg-black text-white px-4 h-[42px] max-h-[42px] text-sm hover:bg-black/80 rounded-lg text-nowrap">Be Pro</button>
+       <button className="lg:flex hidden items-center border border-black px-4 h-[42px] max-h-[42px] hover:bg-black/80 hover:text-white text-sm rounded-lg text-nowrap">Submit Website</button>
+     </div>
     </div>
   )
 }
